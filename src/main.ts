87700e8c79d630071a78d429ff412a053afae9b5
Halyna Urbanska <aquilla@ukr.net>
2023-02-01 14:23:47 +0200

    add fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(3000, '0.0.0.0');
-  Logger.log(`App is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
