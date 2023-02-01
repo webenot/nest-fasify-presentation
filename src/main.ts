b4238d47b8afc6a20bc63d8a5613cbe97ca9e0ca
Halyna Urbanska <aquilla@ukr.net>
2023-02-01 15:53:21 +0200

    add validation pipes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { FastifyRequest } from 'fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -32,6 +33,20 @@ async function bootstrap() {
       requestIdLogLabel: 'traceId',
     })
   );
+
+  app.enableCors();
+
+  const validationPipeOptions = {
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    // after updating class-validator to v0.14.0 pipes throws errors when try to use @Param decorator
+    // that is why we need to turn off this option
+    // Issue https://github.com/nestjs/nest/issues/10683
+    // yarn audit error for lower versions https://github.com/advisories/GHSA-fj58-h2fr-3pp2
+    forbidUnknownValues: false,
+  };
+  app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
 
   const swaggerOptions = new DocumentBuilder()
     .setTitle('App API')
