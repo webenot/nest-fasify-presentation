c7031684af6da5b3d708026b27922e03f680c3aa
Halyna Urbanska <aquilla@ukr.net>
2023-02-01 14:45:26 +0200

    configure fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { FastifyRequest } from 'fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+      genReqId: (request: FastifyRequest) => {
+        return request.headers['x-request-id'] || randomStringGenerator();
+      },
+      requestIdHeader: 'x-request-id',
+      requestIdLogLabel: 'traceId',
+    })
+  );
   await app.listen(3000, '0.0.0.0');
 }
 
