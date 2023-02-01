8b7cc286502be52da8f63065558d6e91399f91c4
Halyna Urbanska <aquilla@ukr.net>
2023-02-01 15:50:54 +0200

    add graphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        context: ({ req, connection }) => ({
+          req: req || connection?.context,
+        }),
+        useGlobalPrefix: true,
+        debug: true,
+        installSubscriptionHandlers: true,
+        subscriptions: {
+          path: '/api/graphql',
+          keepAlive: configService.get('keepAliveMS'),
+          onConnect: (connectionParams) => connectionParams,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
