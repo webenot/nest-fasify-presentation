3111dbdc14a87f8b55e7c916802b1b5543824f3c
Halyna Urbanska <aquilla@ukr.net>
2023-02-01 15:52:42 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { FastifyRequest } from 'fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -31,6 +32,17 @@ async function bootstrap() {
       requestIdLogLabel: 'traceId',
     })
   );
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('App API')
+    .setDescription('Documentation for App API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
 
